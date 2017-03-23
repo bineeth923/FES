5c2dd53d01e82e3674b3c6f7cc398baed58ed974
bineeth923 <bineeth923@gmail.com>
2017-03-23 15:09:27 +0530

    fixed bugs

--- administrator/views.py
+++ administrator/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render, reverse
 from django.http import HttpResponse
 from functions import *
-from teacher import models
+from teacher import models as teacher_view
 from survey import models
 
 @login_required
@@ -17,10 +17,12 @@ def index(request):
 def add_subject(request):
     if request.POST:
         subjectName = request.POST['subject']
-        subject = models.Subject(subjectName=subjectName)
+        subject = teacher_view .Subject(subjectName=subjectName)
         subject.save()
-        return HttpResponseRedirect(reverse('administrator:add_subject') + '?status=sucess')
-    return render(request, 'administrator/add_subject.html')
+        return HttpResponseRedirect(reverse('administrator:add_subject') + '?status=success')
+    subjects = teacher_view.Subject.objects.all()
+    context = {'subjects' : subjects}
+    return render(request, 'administrator/add_subject.html' , context=context)
 
 
 @login_required
@@ -30,12 +32,15 @@ def add_teacher(request):
         username = request.POST['username']
         whichClass = request.POST['whichClass']
         subject = int(request.POST['subject'])
-        teacher = models.Teacher(teacherName=username, Semester=whichClass,
-                                 subject=models.Subject.objects.get(pk=subject))
+        teacher = teacher_view.Teacher(teacherName=username, Semester=whichClass,
+                                 subject=teacher_view.Subject.objects.get(pk=subject))
         teacher.save()
         return HttpResponseRedirect(reverse('administrator:add_teacher') + '?status=success')
-    allSubjects = models.Subject.objects.all()
-    context = {'subjectName': allSubjects}
+    allSubjects = teacher_view.Subject.objects.all()
+    teachers = teacher_view.Teacher.objects.all()
+    context = {'subjectName': allSubjects,
+               'teachers' : teachers
+               }
     return render(request, 'administrator/add_teacher.html', context=context)
 
 
@@ -62,4 +67,14 @@ def survey_creation(request):
     return render(request , 'administrator/survey_creation.html' )
 
 def add_question(request):
-    return
+    if request.POST:
+        if request.POST['mcq']:
+            HttpResponseRedirect(reverse('administrator:add_question_mcq'))
+        elif request.POST['textview']:
+            HttpResponseRedirect(reverse('administrator:add_question_textview'))
+    mcq = models.MCQ.objects.all()
+    mcq_questions = []
+    for mcq in mcq:
+        mcq_questions += [mcq.textName , []]
+    context = {}
+    return render(request , 'administrator/add_question.html' , context=context)
